9b1a91ac8fcb5fe6e4c46efc84acd1f9cd9d16e2
hilton.lssantos <hilton.lssantos@RIP0576973W10-1>
2024-10-09 17:26:55 -0300

    Adicionar arquivos de projeto.

---
+++ DescontoCompra/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DescontoCompra
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal Porcentagem, desconto, valor;
+
+            Console.WriteLine("Informe o preço do produto: ");
+            valor = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informe a Porcentagem de desconto: ");
+            Porcentagem = Convert.ToInt32(Console.ReadLine());
+
+            desconto = valor * (Porcentagem / 100);
+
+            Console.WriteLine("Subtraia o valor do desconto do preço: ");
+            desconto = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Exibir o valor do desconto e o preço final: " + valor);
+            valor = Convert.ToDecimal(Console.ReadLine());
+
+
+            if (valor > 100)
+            {
+                Console.WriteLine("Aplique desconto de 10%");
+            }
+            else
+            {
+                Console.WriteLine("Aplique desconto de 5%");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DescontoCompra/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("DescontoCompra")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DescontoCompra")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("0b06b49a-045c-4922-9894-67abf388cd01")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
